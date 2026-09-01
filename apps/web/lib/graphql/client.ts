bd392edf0ca04c4f0dddde0ecc05e2c3cd94ba71
Ping <tpping@outlook.com>
2026-09-01 09:48:49 +0800

    fix next url

--- apps/web/lib/graphql/client.ts
+++ apps/web/lib/graphql/client.ts
@@ -1,9 +1,9 @@
 import { HttpLink, ApolloClient, InMemoryCache } from "@apollo/client"
 
-const graphqlUrl = process.env.NEXT_PUBLIC_GRAPHQL_URL
+const graphqlUrl = process.env.NEXT_GRAPHQL_URL
 
 if (!graphqlUrl) {
-  throw new Error("NEXT_PUBLIC_GRAPHQL_URL is not defined")
+  throw new Error("NEXT_GRAPHQL_URL is not defined")
 }
 
 export const graphqlClient = new ApolloClient({
